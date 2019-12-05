5a8ef9ca8d17c977edfed403e4a08d2ff724a14b
vialjoris <58552719+vialjoris@users.noreply.github.com>
2019-12-05 14:45:36 +0100

    Update and rename DeleteLog_config.ps1 to DeleteFile_config.ps1

--- DeleteFile_config.ps1
+++ DeleteFile_config.ps1
@@ -1,2 +1,4 @@
 # Number of days to keep | extensions file to include seperate by comma (empty means any extension) | extensions file to exclude seperate by comma | Log folders   ################  Example 1:  2|bak,zip,log,txt,conf|D:\test\    ## Example 2: 7||config|D:\test1
+# exemple : 
+# Garde tous les fichiers qui ont moins de 30j present dans le repertoire D:\LOGS qui n'ont pas pour extesion ".config"
 30||config|D:\LOGS
